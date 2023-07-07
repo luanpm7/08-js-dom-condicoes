4e83979a05d5d5a59ea1d109ee0a52f31bf70972
Luan Paulino Marcelino <132668774+luanpm7@users.noreply.github.com>
2023-07-07 05:32:06 +0000

    metade do sonho

---
+++ script2.js
@@ -0,0 +1,41 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                img.setAttribute('src', 'bebezinho.png')
+            } else if(idade < 21) {
+                img.setAttribute('src', 'jovenzinho.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'adulto.png')
+            } else {
+                img.setAttribute('src', 'idoso.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                img.setAttribute('src', 'bebezinha.png')
+            } else if(idade < 21) {
+                img.setAttribute('src', 'jovenzinha.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'adulta.png')
+            } else {
+                img.setAttribute('src', 'idosa.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
